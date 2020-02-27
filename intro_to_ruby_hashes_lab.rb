46341ba7303907ddb4419436ae30182a78342b60
Julian Wakefield <julianwakefield@outlook.com>
2020-02-27 19:20:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,11 +31,11 @@ end
 
 def update_counting_hash(hash, key)
 counter = 0
-if key["new_key"]
-return hash["new_key"] += 1
+if hash[key]
+return hash[key] += 1
 
 else
-  return hash[new_key: => 1]
+  return hash[key: => 1]
 end
 
 end
