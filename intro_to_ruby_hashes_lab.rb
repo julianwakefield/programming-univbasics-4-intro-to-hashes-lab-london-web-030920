648fbc67ec09fe4239578200113d9f9cda5a5380
Julian Wakefield <julianwakefield@outlook.com>
2020-02-27 15:58:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ id_generator = { :id => 1234}
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+my_hash_creator = {:key => value}
+# return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
